3fa1e3de7b22eb421f07191f1c94b221085fe084
lucass2k <lucasnahuel2000silva@gmail.com>
2023-10-10 02:37:26 -0300

    cart images fix

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import { CartCardStyled } from "../styles/CartCardStyles";
 import { useDispatch } from "react-redux";
 import { removeFromCart } from "../redux/cart/cartSlice";
-const CartCard = ({ quantity, nombre, productImg, precio, id }) => {
+const CartCard = ({ quantity, nombre, productImage, precio, id }) => {
   const dispatch = useDispatch();
   return (
     <>
       <CartCardStyled>
         <div>{quantity}x</div> <div>{nombre}</div> <div>${precio}</div>
-        <img src={productImg} className="imageInCart" alt={nombre} />
+        <img src={productImage} className="imageInCart" alt={nombre} />
         <div onClick={() => dispatch(removeFromCart(id))}>
           <i className="trashCan fa-solid fa-trash"></i>
         </div>
