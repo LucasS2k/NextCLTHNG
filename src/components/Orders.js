a86701232690316c9c0c79906c67dd9af8ae545f
lucass2k <lucasnahuel2000silva@gmail.com>
2023-10-15 17:21:26 -0300

    orders

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,19 +1,45 @@
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/user/userSlice";
 import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Orders = () => {
   const user = useSelector(selectUser);
-  axios
-    .get(`https://next-api-taupe.vercel.app/orders/all`, {
-      headers: {
-        "x-token": user.user.token,
-      },
-    })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      try {
+        const response = await axios.get(
+          `https://next-api-taupe.vercel.app/orders/all`,
+          {
+            headers: {
+              "x-token": user.user.token,
+            },
+          }
+        );
+
+        setData(response.data);
+        console.log(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    }
+    fetchOrders();
+  }, [user.user.token]);
+
+  return (
+    <>
+      {loading ? (
+        <div>Cargando...</div>
+      ) : data.data.length > 0 ? (
+        data.data.map((item) => <div key={item._id}>Valor: ${item.price}</div>)
+      ) : (
+        <div>No hay compras registradas</div>
+      )}
+    </>
+  );
 };
 export default Orders;
