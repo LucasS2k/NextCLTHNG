f8842af16edd943e3416b31d966f1e1c1622e4f3
lucass2k <lucasnahuel2000silva@gmail.com>
2023-10-27 05:05:02 -0300

    show more orders

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -1,5 +1,9 @@
 import { OrderCardStyled } from "../styles/OrderCardStyles";
+import { useState } from "react";
+
 const OrderCard = ({ createdAt, items, price, shippingCost, _id }) => {
+  const [showItems, setShowItems] = useState(false);
+
   const totalQuantity = items
     ? items.reduce((total, item) => total + item.quantity, 0)
     : 0;
@@ -10,13 +14,35 @@ const OrderCard = ({ createdAt, items, price, shippingCost, _id }) => {
     hour: "2-digit",
     minute: "2-digit",
   });
+
+  const toggleItems = () => {
+    setShowItems(!showItems);
+  };
+
   return (
     <OrderCardStyled key={_id}>
-      <span>Productos:{totalQuantity}</span>
+      <span>Productos: {totalQuantity}</span>
       <span>{formattedDate}</span>
       <span>Total: ${price}</span>
-      <span>Costo de envio: ${shippingCost}</span>
+      <span>Costo de envío: ${shippingCost}</span>
+
+      {showItems && (
+        <div>
+          {items.map((item, index) => (
+            <div key={item._id}>
+              <span>{item.nombre}</span>
+              <span>Cantidad: {item.quantity}</span>
+              <span>Precio: ${item.precio}</span>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <button onClick={toggleItems}>
+        {showItems ? "Ocultar detalles" : "Mostrar detalles"}
+      </button>
     </OrderCardStyled>
   );
 };
+
 export default OrderCard;
